4b8902b51b959faae8e4a3393ab25fda11b21ff8
Emilien Schneider <emilien.schneider@gmail.com>
2016-07-22 15:41:14 +0200

    Modification securite fichier

--- wp-content/plugins/restapi-user-basic-access/classes/client.php
+++ wp-content/plugins/restapi-user-basic-access/classes/client.php
@@ -1,6 +1,6 @@
 <?php
 // Blocks direct access
-if ( ! function_exists( 'is_admin' ) || class_exists( 'TestCase' ) ) {
+if ( ! function_exists( 'is_admin' ) && ! class_exists( 'TestCase' ) ) {
 	header( 'Status: 403 Forbidden' );
 	header( 'HTTP/1.1 403 Forbidden' );
 	exit();
