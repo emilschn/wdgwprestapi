a68a40d1fc6f40b31710553136db9d152f617e60
Emilien Schneider <emilien.schneider@gmail.com>
2016-07-22 15:51:16 +0200

    Suppression securite

--- wp-content/plugins/restapi-user-basic-access/classes/client.php
+++ wp-content/plugins/restapi-user-basic-access/classes/client.php
@@ -1,12 +1,4 @@
 <?php
-// Blocks direct access
-if ( ! function_exists( 'is_admin' ) && ! class_exists( 'TestCase' ) ) {
-	header( 'Status: 403 Forbidden' );
-	header( 'HTTP/1.1 403 Forbidden' );
-	exit();
-}
-
-
 class WDG_RESTAPIUserBasicAccess_Class_Client extends WP_User {
     
 	/**
