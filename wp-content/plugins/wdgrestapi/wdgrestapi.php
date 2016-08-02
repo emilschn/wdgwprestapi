ec210d67897e5d0ab1446e5215f16304539209d5
Emilien Schneider <emilien.schneider@gmail.com>
2016-08-02 14:56:05 +0200

    Ajout d'une route pour accéder aux pages statiques qu'on a activer pour l'export

--- wp-content/plugins/wdgrestapi/wdgrestapi.php
+++ wp-content/plugins/wdgrestapi/wdgrestapi.php
@@ -8,6 +8,17 @@
 */
 
 
+/**
+ * How-to:
+ * 	$url = 'http://WP-URL/wp-json/';
+	$route = 'wp/v2/posts/1'; //Route classique wordpress
+	$route2 = 'wdg/v1/staticpages'; //Route vers méthodes spécifiques wdg
+	$id = ID;
+	$pwd = PWD;
+	$headers = array( "Authorization" => "Basic " . base64_encode( $id.':'.$pwd ) );
+	$result = wp_remote_get( $url . $route, array( 'headers' => $headers ) );
+ */
+
 // Vérifie si c'est un appel direct
 if ( ! function_exists( 'is_admin' ) ) {
 	header( 'Status: 403 Forbidden' );
@@ -33,6 +44,9 @@ class WDGRESTAPI {
 	public function __construct() {
 		$this->add_admin_include( 'posts' );
 		WDGRESTAPI_Admin_Posts::add_actions();
+		
+		$this->add_include( 'routes' );
+		add_action( 'rest_api_init', 'WDGRESTAPI_Routes::register');
 	}
 	
 	public function add_include( $include_name ) {
