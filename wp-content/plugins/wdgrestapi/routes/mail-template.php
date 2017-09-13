eecc93071ba9f5881aad54a001a5620cbd156730
emilschn <emilien.schneider@poucr.net>
2017-09-13 14:45:23 +0200

    Templates d'e-mail : récupération de la liste des templates sur SendInBlue

--- wp-content/plugins/wdgrestapi/routes/mail-template.php
+++ wp-content/plugins/wdgrestapi/routes/mail-template.php
@@ -18,11 +18,25 @@ class WDGRESTAPI_Route_MailTemplate extends WDGRESTAPI_Route {
 	 * Retourne la liste des templates de mails exploitables
 	 */
 	public function list_get() {
-		$buffer = array(
-			'template1',
-			'template2',
-			'template3'
+		$buffer = array();
+		
+		include_once( plugin_dir_path( __FILE__ ) . '../libs/sendinblue/mailin.php');
+		$mailin = new Mailin( 'https://api.sendinblue.com/v2.0', WDG_SENDINBLUE_API_KEY, 5000 );
+		$data = array(
+			"type"		=> "template",
+			"status"	=> "temp_active"
 		);
+		$sendinblue_result = $mailin->get_campaigns_v2( $data );
+		
+		$sendinblue_list = $sendinblue_result[ 'data' ][ 'campaign_records' ];
+		foreach ( $sendinblue_list as $sendinblue_template ) {
+			$item = array(
+				'id'	=> $sendinblue_template[ 'id' ],
+				'name'	=> $sendinblue_template[ 'campaign_name' ],
+			);
+			array_push( $buffer, $item );
+		}
+		
 		return $buffer;
 	}
 	
