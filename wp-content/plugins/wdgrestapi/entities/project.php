5b36ec6a7556626cd3a725d9cf2ec5b017a7e44b
emilschn <emilien.schneider@poucr.net>
2017-10-16 17:04:11 +0200

    Projets : ajout de données dans la liste

--- wp-content/plugins/wdgrestapi/entities/project.php
+++ wp-content/plugins/wdgrestapi/entities/project.php
@@ -37,8 +37,45 @@ class WDGRESTAPI_Entity_Project extends WDGRESTAPI_Entity {
 	public static function list_get( $authorized_client_id_string ) {
 		global $wpdb;
 		$table_name = WDGRESTAPI_Entity::get_table_name( WDGRESTAPI_Entity_Project::$entity_type );
-		$query = "SELECT id, wpref, name FROM " .$table_name. " WHERE client_user_id IN " .$authorized_client_id_string;
+		$query = "SELECT * FROM " .$table_name. " WHERE client_user_id IN " .$authorized_client_id_string;
 		$results = $wpdb->get_results( $query );
+		
+		foreach ( $results as $result ) {
+			$result->lw_amount_wallet = 'TODO';
+			$result->lw_authenticated = 'TODO';
+			$result->lw_sepa_signed = 'TODO';
+			$result->roi_declarations_done_count = 'TODO';
+			$result->roi_declarations_total = 'TODO';
+			$result->turnover_total = 'TODO';
+			$result->royalties_total = 'TODO';
+			$result->declarations_start_date = 'TODO';
+			$result->declarations_end_date = 'TODO';
+			$result->organization_name = 'TODO';
+			$result->organization_legal_form = 'TODO';
+			$result->organization_capital = 'TODO';
+			$result->organization_idnumber = 'TODO';
+			$result->organization_vat = 'TODO';
+			$result->organization_address = 'TODO';
+			$result->organization_postal_code = 'TODO';
+			$result->organization_city = 'TODO';
+			$result->organization_country = 'TODO';
+			$result->organization_rcs = 'TODO';
+			$result->organization_representative_firstname = 'TODO';
+			$result->organization_representative_lastname = 'TODO';
+			$result->organization_representative_function = 'TODO';
+			$result->organization_description = 'TODO';
+			$result->organization_fiscal_year_end_month = 'TODO';
+			$result->organization_accounting_contact = 'TODO';
+			$result->organization_iban = 'TODO';
+			$result->organization_bic = 'TODO';
+			$result->organization_document_kbis = 'TODO';
+			$result->organization_document_rib = 'TODO';
+			$result->organization_document_status = 'TODO';
+			$result->organization_document_id = 'TODO';
+			$result->organization_document_home = 'TODO';
+			$result->team_contacts = 'TODO';
+		}
+		
 		return $results;
 	}
 	
@@ -62,7 +99,37 @@ class WDGRESTAPI_Entity_Project extends WDGRESTAPI_Entity {
 		'wpref'					=> array( 'type' => 'id', 'other' => 'NOT NULL' ),
 		'client_user_id'		=> array( 'type' => 'id', 'other' => 'DEFAULT 1 NOT NULL' ),
 		'creation_date'			=> array( 'type' => 'date', 'other' => '' ),
-		'name'					=> array( 'type' => 'varchar', 'other' => 'NOT NULL' )
+		'name'					=> array( 'type' => 'varchar', 'other' => 'NOT NULL' ),
+		'status'				=> array( 'type' => 'varchar', 'other' => 'NOT NULL' ),
+		'type'					=> array( 'type' => 'varchar', 'other' => 'NOT NULL' ),
+		'category'				=> array( 'type' => 'varchar', 'other' => 'NOT NULL' ),
+		'impacts'				=> array( 'type' => 'varchar', 'other' => 'NOT NULL' ),
+		'amount_collected'		=> array( 'type' => 'float', 'other' => 'NOT NULL' ),
+		'roi_percent_estimated'	=> array( 'type' => 'float', 'other' => 'NOT NULL' ),
+		'roi_percent'			=> array( 'type' => 'float', 'other' => 'NOT NULL' ),
+		'estimated_budget_file'		=> array( 'type' => 'varchar', 'other' => 'NOT NULL' ),
+		'funding_duration'		=> array( 'type' => 'int', 'other' => 'NOT NULL' ),
+		'goal_minimum'			=> array( 'type' => 'int', 'other' => 'NOT NULL' ),
+		'goal_maximum'			=> array( 'type' => 'int', 'other' => 'NOT NULL' ),
+		'yield_for_investors'	=> array( 'type' => 'float', 'other' => 'NOT NULL' ),
+		'maximum_profit'		=> array( 'type' => 'int', 'other' => 'NOT NULL' ),
+		'minimum_profit'		=> array( 'type' => 'int', 'other' => 'NOT NULL' ),
+		'contract_start_date'	=> array( 'type' => 'date', 'other' => '' ),
+		'spendings_description'	=> array( 'type' => 'longtext', 'other' => '' ),
+		'earnings_description'	=> array( 'type' => 'longtext', 'other' => '' ),
+		'simple_info'			=> array( 'type' => 'longtext', 'other' => '' ),
+		'detailed_info'			=> array( 'type' => 'longtext', 'other' => '' ),
+		'estimated_turnover'	=> array( 'type' => 'varchar', 'other' => '' ),
+		'blank_contract_file'	=> array( 'type' => 'varchar', 'other' => '' ),
+		'vote_start_datetime'	=> array( 'type' => 'datetime', 'other' => '' ),
+		'vote_end_datetime'		=> array( 'type' => 'datetime', 'other' => '' ),
+		'vote_count'			=> array( 'type' => 'int', 'other' => '' ),
+		'vote_invest_amount'	=> array( 'type' => 'float', 'other' => '' ),
+		'funding_start_datetime'	=> array( 'type' => 'datetime', 'other' => '' ),
+		'funding_end_datetime'	=> array( 'type' => 'datetime', 'other' => '' ),
+		'investments_count'		=> array( 'type' => 'int', 'other' => '' ),
+		'costs_to_organization'	=> array( 'type' => 'float', 'other' => '' ),
+		'costs_to_investors'	=> array( 'type' => 'float', 'other' => '' ),
 	);
 	
 	// Mise à jour de la bdd
--- wp-content/plugins/wdgrestapi/wdgrestapi.php
+++ wp-content/plugins/wdgrestapi/wdgrestapi.php
@@ -28,7 +28,7 @@ if ( ! function_exists( 'is_admin' ) ) {
 
 
 class WDGRESTAPI {
-	private $version = '0.0.715';
+	private $version = '0.0.72';
     
 	/**
 	 * Instanciation du singleton
