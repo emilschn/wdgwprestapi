ff4777d151a0cd9aa14d02a1eff0386622bdafaf
emilschn <emilien.schneider@poucr.net>
2019-08-02 14:47:01 +0200

    Tests unitaires : Validator : suite corrections de tests

--- wp-content/plugins/wdgrestapi/libs/validator.php
+++ wp-content/plugins/wdgrestapi/libs/validator.php
@@ -305,7 +305,7 @@ class WDGRESTAPI_Lib_Validator {
 	 * @return boolean
 	 */
 	public static function is_number_integer( $input ) {
-		return ( self::is_number( $input ) && (int) $input === round( (int) $input ) );
+		return ( self::is_number( $input ) && $input == round( $input ) );
 	}
 	
 	/**
@@ -323,7 +323,7 @@ class WDGRESTAPI_Lib_Validator {
 	 * @return boolean
 	 */
 	public static function is_email( $input ) {
-		return ( filter_var( $input, FILTER_VALIDATE_EMAIL ) != FALSE );
+		return ( filter_var( $input, FILTER_VALIDATE_EMAIL ) !== FALSE );
 	}
 	
 	/**
@@ -332,7 +332,7 @@ class WDGRESTAPI_Lib_Validator {
 	 * @return boolean
 	 */
 	public static function is_name( $input ) {
-		return !empty( $input ) && !self::is_number( $input ) && !self::is_email( $input );
+		return !empty( $input ) && !is_bool( $input ) && !self::is_number( $input ) && !self::is_email( $input );
 	}
 	
 	/**
@@ -429,6 +429,9 @@ class WDGRESTAPI_Lib_Validator {
 	 */
 	public static function is_postalcode( $input, $input_country ) {
 		$input = str_replace( ' ', '', $input );
+		if ( $input_country == 'FR' && strlen( $input ) == 4 ) {
+			$input = '0' . $input;
+		}
 		return ( $input_country != 'FR' || preg_match( '#^[0-9]{5}$#', $input ) );
 	}
 	
@@ -438,7 +441,7 @@ class WDGRESTAPI_Lib_Validator {
 	 * @return boolean
 	 */
 	public static function is_minimum_amount( $input ) {
-		return ( $input >= WDGRESTAPI_Lib_Validator::$minimum_amount );
+		return ( self::is_number_positive_integer( $input ) && $input >= self::$minimum_amount );
 	}
 	
 	/**
@@ -460,11 +463,15 @@ class WDGRESTAPI_Lib_Validator {
 		$countries = array('al'=>28,'ad'=>24,'at'=>20,'az'=>28,'bh'=>22,'be'=>16,'ba'=>20,'br'=>29,'bg'=>22,'cr'=>21,'hr'=>21,'cy'=>28,'cz'=>24,'dk'=>18,'do'=>28,'ee'=>20,'fo'=>18,'fi'=>18,'fr'=>27,'ge'=>22,'de'=>22,'gi'=>23,'gr'=>27,'gl'=>18,'gt'=>28,'hu'=>28,'is'=>26,'ie'=>22,'il'=>23,'it'=>27,'jo'=>30,'kz'=>20,'kw'=>30,'lv'=>21,'lb'=>28,'li'=>21,'lt'=>20,'lu'=>20,'mk'=>19,'mt'=>31,'mr'=>27,'mu'=>30,'mc'=>27,'md'=>24,'me'=>22,'nl'=>18,'no'=>15,'pk'=>24,'ps'=>29,'pl'=>28,'pt'=>25,'qa'=>29,'ro'=>24,'sm'=>27,'sa'=>24,'rs'=>22,'sk'=>24,'si'=>19,'es'=>24,'se'=>24,'ch'=>21,'tn'=>24,'tr'=>26,'ae'=>23,'gb'=>22,'vg'=>24);
 		$chars = array('a'=>10,'b'=>11,'c'=>12,'d'=>13,'e'=>14,'f'=>15,'g'=>16,'h'=>17,'i'=>18,'j'=>19,'k'=>20,'l'=>21,'m'=>22,'n'=>23,'o'=>24,'p'=>25,'q'=>26,'r'=>27,'s'=>28,'t'=>29,'u'=>30,'v'=>31,'w'=>32,'x'=>33,'y'=>34,'z'=>35);
 
-		if ( strlen( $iban ) < 2 && !isset( $countries[ substr( $iban, 0, 2 ) ] ) ) {
+		if ( strlen( $iban ) < 2 ) {
+			return FALSE;
+		}
+		$country_key = substr( $iban, 0, 2 );
+		if ( !array_key_exists( $country_key, $countries ) ) {
 			return FALSE;
 		}
 
-		if ( strlen($iban) == $countries[ substr( $iban, 0, 2 ) ] ) {
+		if ( strlen($iban) == $countries[ $country_key ] ) {
 			$moved_char = substr( $iban, 4 ).substr( $iban, 0, 4 );
 			$moved_char_array = str_split( $moved_char );
 			$new_string = "";
