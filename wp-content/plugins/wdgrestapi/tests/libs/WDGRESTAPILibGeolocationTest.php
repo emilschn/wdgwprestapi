2894fe0d180b50df39a53dc5e162c964aceba1d2
emilschn <emilien.schneider@poucr.net>
2019-09-03 15:38:43 +0200

    Tests unitaires : ajout test geolocation

---
+++ wp-content/plugins/wdgrestapi/tests/libs/WDGRESTAPILibGeolocationTest.php
@@ -0,0 +1,22 @@
+<?php
+require_once dirname( __FILE__ ) . '/../../../../../wp-includes/load.php';
+require_once dirname( __FILE__ ) . '/../../../../../wp-includes/plugin.php';
+require_once dirname( __FILE__ ) . '/../../../../../wp-includes/options.php';
+require_once dirname( __FILE__ ) . '/../../../../../wp-includes/http.php';
+require_once dirname( __FILE__ ) . '/../../../../../wp-includes/default-constants.php';
+require_once dirname( __FILE__ ) . '/../../../../../wp-includes/class-wp-error.php';
+require_once dirname( __FILE__ ) . '/../../libs/geolocation.php';
+
+class WDGRESTAPILibGeolocationTest extends PHPUnit_Framework_TestCase {
+
+	public function testgetGeolocationData() {
+		$this->assertEmpty( WDGRESTAPI_Lib_Geolocation::get_geolocation_data( '' ) );
+		
+		$geoloc_rubbish = WDGRESTAPI_Lib_Geolocation::get_geolocation_data( 'blabliblou blabliblou' );
+		$this->assertEquals( TRUE, is_wp_error( $geoloc_rubbish ) );
+
+		$geoloc_existing = WDGRESTAPI_Lib_Geolocation::get_geolocation_data( '1 place de la bourse 44000 Nantes' );
+		$this->assertEquals( TRUE, ( isset( $geoloc_existing[ 'lat' ] ) && isset( $geoloc_existing[ 'long' ] ) ) );
+	}
+	
+}
