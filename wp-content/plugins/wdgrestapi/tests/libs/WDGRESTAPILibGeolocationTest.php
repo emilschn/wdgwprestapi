8e747833c9ec85bafe6b80c26e70343ae3754420
emilschn <emilien.schneider@poucr.net>
2019-09-03 16:31:19 +0200

    Tests unitaires : geolocation : ajout d'includes

--- wp-content/plugins/wdgrestapi/tests/libs/WDGRESTAPILibGeolocationTest.php
+++ wp-content/plugins/wdgrestapi/tests/libs/WDGRESTAPILibGeolocationTest.php
@@ -1,16 +1,19 @@
 <?php
-require_once dirname( __FILE__ ) . '/../../../../../wp-includes/cache.php';
 require_once dirname( __FILE__ ) . '/../../../../../wp-includes/load.php';
 require_once dirname( __FILE__ ) . '/../../../../../wp-includes/plugin.php';
+require_once dirname( __FILE__ ) . '/../../../../../wp-includes/cache.php';
 require_once dirname( __FILE__ ) . '/../../../../../wp-includes/option.php';
 require_once dirname( __FILE__ ) . '/../../../../../wp-includes/http.php';
 require_once dirname( __FILE__ ) . '/../../../../../wp-includes/default-constants.php';
 require_once dirname( __FILE__ ) . '/../../../../../wp-includes/class-wp-error.php';
+require_once dirname( __FILE__ ) . '/../../../../../wp-includes/class-wp-hook.php';
 require_once dirname( __FILE__ ) . '/../../libs/geolocation.php';
 
 class WDGRESTAPILibGeolocationTest extends PHPUnit_Framework_TestCase {
 
 	public function testgetGeolocationData() {
+		wp_start_object_cache();
+
 		$this->assertEmpty( WDGRESTAPI_Lib_Geolocation::get_geolocation_data( '' ) );
 		
 		$geoloc_rubbish = WDGRESTAPI_Lib_Geolocation::get_geolocation_data( 'blabliblou blabliblou' );
